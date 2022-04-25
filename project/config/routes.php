c5149884a61010d9e8cd0afcc852a6dfe2e9f68d
djetrl <82327079+djetrl@users.noreply.github.com>
2022-04-25 10:40:07 +0700

    Add files via upload

--- project/config/routes.php
+++ project/config/routes.php
@@ -2,7 +2,12 @@
 	use \Core\Route;
 	
 	return [
-		new Route('/page/:id', 'page', 'one'),
-		new Route('/pages',   'page', 'all'),
+		new Route('/product/:n/', 'product', 'show'), 
+		new Route('/product/all/', 'product', 'all'),
+		new Route('/page/:id/', 'page', 'show'),
+		new Route('/user/:id/:key/', 'user', 'info'), 
+		// new Route('/user/:id/', 'user', 'show'),
+		// new Route('/user/all/', 'user', 'all'),
+		// new Route('/user/first/:n/', 'user', 'first'),
 	];
 ?>
--- project/layouts/default.php
+++ project/layouts/default.php
@@ -2,28 +2,26 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<link rel="stylesheet" href="/1804/project/webroot/styles.css">
 		<title><?= $title ?></title> 
 			<!-- выводим тайтл страницы --> 
 	</head>
 	<body>
 		<header>
-		
+			хедер сайта
 		</header>
 		<div class="container">
 			<aside class="sidebar left">
-			
+				левый сайдбар
 			</aside>
 			<main>
 				<?= $content ?>
-				<!-- <img src="\1804\project\webroot\на ките.jpg" alt="альтернативный текст"> -->
 			</main>
 			<aside class="sidebar right">
-			
+				правый сайдбар
 			</aside>
 		</div>
 		<footer>
-		
+			футер сайта
 		</footer>
 	</body>
 </html>
